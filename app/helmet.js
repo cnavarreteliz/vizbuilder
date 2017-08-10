b864c058d56d8958f696c24dff58d321dfa666d6
Carlos Navarrete <cnavarretel@users.noreply.github.com>
2017-08-09 21:49:41 -0400

    Add helmet.js

---
+++ app/helmet.js
@@ -0,0 +1,20 @@
+module.exports = {
+    link: [
+        {rel: "stylesheet", href: "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"}
+    ],
+    meta: [
+        {charset: "utf-8"},
+        {"http-equiv": "X-UA-Compatible", "content": "IE=edge"},
+        {name: "description", content: "An experiment for Canon."},
+        {name: "viewport", content: "width=device-width, initial-scale=1"},
+        {name: "mobile-web-app-capable", content: "yes"},
+        {name: "apple-mobile-web-app-capable", content: "yes"},
+        {name: "apple-mobile-web-app-status-bar-style", content: "black"},
+        {name: "apple-mobile-web-app-title", content: "VizBuilder"},
+        {property: "og:title", content: "VizBuilder"},
+        {property: "og:image", content: "https://dataafrica.io/images/africa-share.jpg"},
+        {property: "og:description", content: "An experiment for Canon."},
+        {property: "fb:app_id", content: "260789081071557"}
+    ],
+    title: "Viz Builder Datawheel CANON"
+};
