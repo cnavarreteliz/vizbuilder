d100001b350e86443446d34a78964fe0c3798df9
Francisco <git.id@frabarz.cl>
2017-08-19 03:59:07 -0300

    Redux enabled for filters and data

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Button } from "@blueprintjs/core";
+import { Button } from "@blueprintjs/core/dist/components/button/buttons.js";
 
-import countries from "data/countries.json";
 import properties from "data/properties.json";
 import { getData } from "helpers/stats";
 
 import Filter, { defaultFilter, applyFilters } from "components/Filter";
 
-import Selector from "components/Selector";
 import VizSelector from "components/VizSelector";
 import YearSelector from "components/YearSelector";
 import VizBuilder from "components/VizBuilder";
@@ -20,7 +18,6 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			data: [],
 			viz: {
 				type: "treemap",
 				panel: true
@@ -37,9 +34,6 @@ class App extends Component {
 		this.handleChangeAxis = this.handleChangeAxis.bind(this);
 		this.handleChangeMeasure = this.handleChangeMeasure.bind(this);
 		this.getKSAdata = this.getKSAdata.bind(this);
-		this.handleFilterAdd = this.handleFilterAdd.bind(this);
-		this.handleFilterUpdate = this.handleFilterUpdate.bind(this);
-		this.handleFilterRemove = this.handleFilterRemove.bind(this);
 
 		this.getKSAdata(
 			"Employee Records",
@@ -61,27 +55,21 @@ class App extends Component {
 	async getDataOptions(cubeName, dimension, measure) {
 		let axis = await getData(cubeName, dimension, measure).promise;
 		let data = axis[0].measures.map(item => item.name);
-		// console.log(data);
 		return data;
 	}
 
-	getKSAdata(cubeName, dimension, measure) {
-		// Experiments
-		getData(cubeName, dimension, measure).promise.then(data => {
-			// console.log(data);
-			const ksa_data = data[1].values.map((level1, i) => {
-				return {
-					year: 2016,
-					value: parseFloat(level1[0][0]),
-					id: i + 1,
-					name: data[1].axes[2].members[i].name,
-					group: data[1].axes[2].members[i].name[0]
-				};
-			});
-			this.setState({
-				data: ksa_data
-			});
-		});
+	async getKSAdata(cubeName, dimension, measure) {
+		let data = await getData(cubeName, dimension, measure).promise;
+
+		let ksa_data = data[1].values.map((level1, i) => ({
+			year: 2016,
+			value: parseFloat(level1[0][0]),
+			id: i + 1,
+			name: data[1].axes[2].members[i].name,
+			group: data[1].axes[2].members[i].name[0]
+		}));
+
+		this.props.onDataLoad(ksa_data);
 	}
 
 	getProperty(data, attribute) {
@@ -124,34 +112,21 @@ class App extends Component {
 		});
 	}
 
-	handleFilterAdd() {
-		this.props.dispatch({ type: "FILTER_ADD", payload: defaultFilter() });
-	}
-
-	handleFilterUpdate(id, props) {
-		this.props.dispatch({ type: "FILTER_UPDATE", payload: { ...props, _id: id } });
-	}
-
-	handleFilterRemove(index) {
-		this.props.dispatch({ type: "FILTER_DELETE", payload: index });
-	}
-
-	renderFilters() {
-		// let columns = Object.keys(this.state.data[0]);
-		let columns = [];
-
-		return this.props.filters.map(function(filter, idx) {
-			return <Filter
-				key={idx}
-				columns={columns}
-				property={filter.property}
-				operator={filter.operator}
-				value={filter.value}
-				index={idx}
-				onChange={this.handleFilterUpdate}
-				onDelete={this.handleFilterRemove}
-			/>
-		}, this);
+	renderFilters(props) {
+		return props.filters.map(function(filter, idx) {
+			return (
+				<Filter
+					key={filter._id}
+					columns={props.dataColumns}
+					property={filter.property}
+					operator={filter.operator}
+					value={filter.value}
+					index={idx}
+					onChange={props.onFilterUpdate}
+					onDelete={props.onFilterRemove}
+				/>
+			);
+		});
 	}
 
 	render() {
@@ -169,20 +144,20 @@ class App extends Component {
 							size={this.state.measure}
 							dimension={this.state.dimension}
 						/>
-						{this.renderFilters.call(this)}
+						{this.renderFilters(this.props)}
 						<Button
 							className="pt-fill"
 							iconName="add"
-							onClick={this.handleFilterAdd}
+							onClick={this.props.onFilterAdd}
 						>
 							Add filter
 						</Button>
 					</div>
 					<div className="viz-wrapper">
 						<VizBuilder
-							type={this.state.viz.type}
+							title="Hello"
 							config={{
-								data: applyFilters(this.state.data, this.props.filters || []),
+								data: this.props.data,
 								title: "Hello",
 								size: this.state.measure,
 								groupBy: ["group", "name"]
@@ -198,9 +173,32 @@ class App extends Component {
 
 function mapStateToProps(state) {
 	return {
+		dataColumns: Object.keys(state.data.rawData[0] || {}),
 		filters: state.filters
 	};
 }
 
-export default connect(mapStateToProps)(App);
-// export default App;
+function mapDispatchToProps(dispatch) {
+	return {
+		onDataLoad(data) {
+			dispatch({ type: "DATA_UPDATE", payload: data });
+		},
+
+		onFilterAdd() {
+			dispatch({ type: "FILTER_ADD", payload: defaultFilter() });
+		},
+
+		onFilterUpdate(id, props) {
+			dispatch({
+				type: "FILTER_UPDATE",
+				payload: { ...props, _id: id }
+			});
+		},
+
+		onFilterRemove(index) {
+			dispatch({ type: "FILTER_DELETE", payload: index });
+		}
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
