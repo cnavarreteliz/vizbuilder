dba26370af268f953bc2b5a8bb99ffa7a3a15c96
Carlos Navarrete <cnavarretel@users.noreply.github.com>
2017-08-24 00:18:40 -0300

    Fix Assistant options

--- app/components/Assistant.jsx
+++ app/components/Assistant.jsx
@@ -5,18 +5,60 @@ function Assistant(props) {
 	return (
 		<div className="panel-assistant">
 			<h2>Assistant</h2>
-			<p>Hello, I'm Liz, your data analytics assistant.</p>
+			<p>Hello, I'm Ann, your data analytics assistant.</p>
 			<hr />
 			<h5>Suggested searches:</h5>
 			<ul>
 				<li>
-					<a href="#" onClick={evt => props.onClickAssistant("Salary Average")}>
+					<a
+						href="#"
+						onClick={evt =>
+							props.onClickAssistant(
+								"Employee Records",
+								"Occupation",
+								"Salary Average"
+							)}
+					>
 						Salary Average in Occupations
 					</a>
 				</li>
 				<li>
-					<a href="#" onClick={evt => props.onClickAssistant("Record Count")}>
+					<a
+						href="#"
+						onClick={evt =>
+							props.onClickAssistant(
+								"Employee Records",
+								"Occupation",
+								"Record Count"
+							)}
+					>
 						Record Count in Occupations
+					</a>
+				</li>
+				<li>
+					<a
+						href="#"
+						onClick={evt =>
+							props.onClickAssistant(
+								"Business Records",
+								"Industry",
+								"Business Count"
+							)}
+					>
+						Number of business by Industry
+					</a>
+				</li>
+				<li>
+					<a
+						href="#"
+						onClick={evt =>
+							props.onClickAssistant(
+								"Education Local",
+								"Degree",
+								"Student Count"
+							)}
+					>
+						Number of students by Degree
 					</a>
 				</li>
 			</ul>
@@ -26,8 +68,8 @@ function Assistant(props) {
 
 function mapDispatchToProps(dispatch) {
 	return {
-		onClickAssistant(measure) {
-			dispatch({ type: "AXIS_UPDATE", measure });
+		onClickAssistant(cube, dimension, measure) {
+			dispatch({ type: "AXIS_UPDATE", cube, dimension, measure });
 			return false;
 		}
 	};
