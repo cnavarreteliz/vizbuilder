3d9495b0226f7537950a8e802c927d4f02c70abe
Carlos Navarrete <cnavarretel@users.noreply.github.com>
2017-09-27 14:40:09 -0300

    prepare measure formatters

--- app/helpers/d3plus.js
+++ app/helpers/d3plus.js
@@ -1,3 +1,5 @@
+import { FORMATTERS } from "helpers/formatters";
+
 const axisConfig = {
 	barConfig: {
 		stroke: "#aaaaaa",
@@ -25,6 +27,7 @@ const axisConfig = {
 };
 
 export const CHARTCONFIG = {
+	barPadding: 10,
 	legendConfig: {
 		marginLeft: 50,
 		padding: 8,
@@ -65,23 +68,28 @@ export const CHARTCONFIG = {
 		padding: "10px",
 		width: "200px",
 		background: "white",
-		title: d => d.name,
+		title: d => d.id,
 		titleStyle: {
 			"font-size": "18px",
 			"text-transform": "uppercase",
 			"font-weight": "bold",
-			"font-family": "'Pathway Gothic One', sans-serif",
-			"margin-bottom": "12px"
+			"text-align": "center",
+			"font-family": "'Work Sans', sans-serif"
 		},
 		body(item) {
 			item = item.source;
 			return Object.keys(item).reduce(function(html, key) {
 				let value = isNumeric(item[key])
-					? item[key]
+					? abbreviateFormat(key, item[key])
 					: item[key];
-				html += "<div class='tooltip-row'>" + key + ": " + value + "</div>";
+				html +=
+					"<div class='tooltip-item'><div class='title'>" +
+					key +
+					"</div><div class='content'>" +
+					value +
+					"</div></div>";
 				return html;
-			}, "");
+			}, "<div class='header-line'></div>");
 		},
 		bodyStyle: {
 			"font-size": "16px"
@@ -101,21 +109,14 @@ function isNumeric(n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-function abbreviateNumber(num, fixed = 0) {
-	if (num === null) {
-		return null;
-	} // terminate early
-	if (num === 0) {
-		return "0";
-	} // terminate early
-	fixed = !fixed || fixed < 0 ? 0 : fixed; // number of decimal places to show
-	var b = num.toPrecision(2).split("e"), // get power
-		k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
-		c =
-			k < 1
-				? num.toFixed(0 + fixed)
-				: (num / Math.pow(10, k * 3)).toFixed(1 + fixed), // divide by power
-		d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
-		e = d + ["", "K", "M", "B", "T"][k]; // append power
-	return e;
+function abbreviateFormat(name, value) {
+	if (name.includes("Salary")) {
+		return FORMATTERS.dollarCommas(value);
+	} else if(name === "Growth") {
+		let span_class = value >= 0 ? "increase" : "decrease";
+		value = FORMATTERS.share(value);
+		return "<span class=" + span_class + ">" + value +"</span>"
+	} else {
+		return FORMATTERS.commas(value);
+	}
 }
