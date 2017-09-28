7647aa5144b5a761f02eb9cbb3def10b85a738df
Francisco <git.id@frabarz.cl>
2017-09-27 23:46:32 -0300

    Cleaning components

--- app/components/InputPopover.jsx
+++ app/components/InputPopover.jsx
@@ -5,36 +5,6 @@ import { Popover } from "@blueprintjs/core/dist/components/popover/popover";
 import { Menu } from "@blueprintjs/core/dist/components/menu/menu";
 import { MenuItem } from "@blueprintjs/core/dist/components/menu/menuItem";
 
-function InputPopover(props) {
-	function createMenuItem(item) {
-		let children = null,
-			attr = { key: item.key, text: item.label };
-
-		if (Array.isArray(item.value)) {
-			children = item.value.map(createMenuItem);
-		} else {
-			attr.onClick = () => props.onClick(item.value);
-		}
-
-		return React.createElement(MenuItem, attr, children);
-	}
-
-	let position =
-		"string" === typeof props.position
-			? Position[props.position]
-			: props.position;
-
-	return (
-		<Popover
-			target={props.value}
-			content={
-				<Menu>{props.options.sort(sortParentsFirst).map(createMenuItem)}</Menu>
-			}
-			position={position}
-		/>
-	);
-}
-
 InputPopover.propTypes = {
 	options: PropTypes.array.isRequired,
 	value: PropTypes.string,
@@ -48,8 +18,42 @@ InputPopover.defaultProps = {
 	position: Position.BOTTOM
 };
 
-function sortParentsFirst(a,b) {
-	return (a.value.length || 0) < (b.value.length || 0)
+function InputPopover(props) {
+	let position =
+		"string" === typeof props.position
+			? Position[props.position]
+			: props.position;
+
+	return (
+		<Popover
+			target={props.value}
+			content={
+				<Menu>
+					{props.options
+						.sort(sortParentsFirst)
+						.map(option => createMenuItem(option, props.onClick))}
+				</Menu>
+			}
+			position={position}
+		/>
+	);
+}
+
+function createMenuItem(item, onClick) {
+	let children = null,
+		attr = { key: item.key, text: item.label };
+
+	if (Array.isArray(item.value)) {
+		children = item.value.map(createMenuItem);
+	} else {
+		attr.onClick = () => onClick(item.value);
+	}
+
+	return React.createElement(MenuItem, attr, children);
+}
+
+function sortParentsFirst(a, b) {
+	return (a.value.length || 0) < (b.value.length || 0);
 }
 
 export default InputPopover;
