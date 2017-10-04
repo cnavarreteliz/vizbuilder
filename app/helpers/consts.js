701b844542cd4a9abc0f69fdb318973391666358
Carlos Navarrete <cnavarretel@users.noreply.github.com>
2017-10-04 16:15:05 -0300

    clean year consts

--- app/helpers/consts.js
+++
@@ -1,6 +0,0 @@
-//export const API = "https://dataksa-cube.datawheel.us/";
-//export const API = "https://jobsksa-monet.datawheel.us/";
-export const API = "https://chilecube.datawheel.us/";
-export const CUBE_API = API;
-export const FIRST_YEAR = 2009;
-export const LATEST_YEAR = 2016;
