460adb8d5f52ade43da64cb0d6bf0b4d67f56680
Francisco <git.id@frabarz.cl>
2017-11-20 16:15:52 -0300

    Changing KSA dataset's URL

--- app/pages/DataKsa.jsx
+++ app/pages/DataKsa.jsx
@@ -8,7 +8,7 @@ function DataKsa(props) {
 	return (
 		<div className="container data-ksa">
 			<LoadControl
-				src="https://jobsksa-monet.datawheel.us/"
+				src="https://jobsksa.datawheel.us/"
 				queryString={props.location.search}
 			/>
 			<AreaSidebar />
